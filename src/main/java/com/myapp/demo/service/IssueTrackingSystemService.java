d94b2dee8c84f301564c6081aee129c33d535197
attila-m <at.murvai@gmail.com>
2020-08-20 12:35:13 +0200

    add calculateduedate test and method

--- src/main/java/com/myapp/demo/service/IssueTrackingSystemService.java
+++ src/main/java/com/myapp/demo/service/IssueTrackingSystemService.java
@@ -1,4 +1,15 @@
 package com.myapp.demo.service;
 
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+@Service
 public class IssueTrackingSystemService {
+
+    public LocalDateTime CalculateDueDate(LocalDateTime submissionDate, Duration turnaroundTime) {
+        return submissionDate.plus(turnaroundTime);
+    }
+
 }
--- src/test/java/com/myapp/demo/CalculateDueDateTest.java
+++ src/test/java/com/myapp/demo/CalculateDueDateTest.java
@@ -1,13 +1,35 @@
 package com.myapp.demo;
 
+import com.myapp.demo.service.IssueTrackingSystemService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.Month;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class CalculateDueDate {
 
+    @Autowired
+    IssueTrackingSystemService issueTrackingSystemService;
+
+    private static final LocalDateTime SUBMISSION_DATE = LocalDateTime.of(2020,
+            Month.AUGUST,
+            20,
+            9,
+            0);
+    private static final Duration TURNAROUND_TIME = Duration.ofHours(7);
+
     @Test
-    void contextLoads() {
+    public void ShouldReturnSubmissionDatePlusTurnaroundTime() {
+        LocalDateTime resolveDate = issueTrackingSystemService.CalculateDueDate(SUBMISSION_DATE, TURNAROUND_TIME);
+        LocalDateTime expectedResolveDate = LocalDateTime.of(2020, Month.AUGUST, 20, 16, 0);
+
+        assertEquals(resolveDate, expectedResolveDate);
     }
 
 }
