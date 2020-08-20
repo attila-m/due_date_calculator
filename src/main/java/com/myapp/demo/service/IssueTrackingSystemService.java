d264500a194efdefc244ce54c0eba55ca2d1c7eb
attila-m <at.murvai@gmail.com>
2020-08-20 16:24:19 +0200

    add resolveday calculator test and method

--- src/main/java/com/myapp/demo/service/IssueTrackingSystemService.java
+++ src/main/java/com/myapp/demo/service/IssueTrackingSystemService.java
@@ -12,32 +12,78 @@ import java.time.LocalDateTime;
 @Service
 public class IssueTrackingSystemService {
 
+    public static final long WORK_STARTS_AT = 9;
+    public static final long WORK_ENDS_AT = 17;
+    public static final long WORK_HOURS = 8;
+
     private static final Logger LOGGER = LogManager.getLogger(IssueTrackingSystemService.class);
 
     public LocalDateTime CalculateDueDate(LocalDateTime submissionDate, Duration turnaroundTime) throws CalculateDueDateException{
+
+        LocalDateTime resolveDate = submissionDate;
+
         validateSubmissionDate(submissionDate);
-        return submissionDate.plus(turnaroundTime);
+        validateTurnaroundTime(turnaroundTime);
+
+        resolveDate = resolveDate
+                .plusDays(calculateWorkdays(submissionDate.getDayOfWeek(), turnaroundTime))
+                .plusHours(turnaroundTime.toHours() % 8);
+
+        return resolveDate;
+    }
+
+    public long calculateWorkdays(DayOfWeek dayOfWeek, Duration turnaroundTime) {
+        long fullDays = turnaroundTime.toHours() / WORK_HOURS;
+
+        DayOfWeek currentDay = dayOfWeek;
+        long fullWorkDays = 0;
+
+        for (int i = 0; i < fullDays; i++) {
+            if (!isWeekendDay(currentDay)) {
+                fullWorkDays++;
+            }
+            currentDay.plus(1);
+        }
+        return fullWorkDays;
     }
 
     public void validateSubmissionDate(LocalDateTime submissionDate) throws CalculateDueDateException {
         if(!isDuringWorkingHours(submissionDate)) {
-            String errorMessage = submissionDate + " is not a workday.";
+            String errorMessage = submissionDate + " is not during working hours. Working hours are between 9AM to 5PM, from Monday to Friday.";
             LOGGER.error(errorMessage);
             throw new CalculateDueDateException(errorMessage);
         }
     }
 
-    public boolean isDuringWorkingHours(LocalDateTime date) {
+    public void validateTurnaroundTime(Duration turnaroundTime) throws CalculateDueDateException {
+        if(!isTurnaroundTimeGreaterThanZero(turnaroundTime)) {
+            String errorMessage = turnaroundTime + " is not a valid value. Turnaround time should be more than zero.";
+            LOGGER.error(errorMessage);
+            throw new CalculateDueDateException(errorMessage);
+        }
+    }
 
-        if (date.getDayOfWeek() == DayOfWeek.SATURDAY || date.getDayOfWeek() == DayOfWeek.SUNDAY ) {
+    private boolean isTurnaroundTimeGreaterThanZero(Duration turnaroundTime) {
+        if (turnaroundTime.isNegative() || turnaroundTime.isZero()) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isDuringWorkingHours(LocalDateTime date) {
+
+        if (isWeekendDay(date.getDayOfWeek())) {
             return false;
         }
 
-        if (date.getHour() < 9 || date.getHour() > 16) {
+        if (date.getHour() < WORK_STARTS_AT || date.getHour() > WORK_ENDS_AT - 1) {
             return false;
         }
 
         return true;
     }
 
+    private boolean isWeekendDay(DayOfWeek dayOfWeek) {
+        return dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY;
+    }
 }
--- src/test/java/com/myapp/demo/CalculateDueDateTest.java
+++ src/test/java/com/myapp/demo/CalculateDueDateTest.java
@@ -20,6 +20,8 @@ class CalculateDueDate {
 
     private static final LocalDateTime SUBMISSION_DATE = LocalDateTime.of(2020, Month.AUGUST, 20, 9, 0);
     private static final Duration TURNAROUND_TIME = Duration.ofHours(7);
+    private static final Duration TURNAROUND_TIME_FULL_WORKDAY = Duration.ofHours(8);
+    private static final Duration TURNAROUND_TIME_TWO_FULL_WORKDAY = Duration.ofHours(16);
 
     @Test
     public void ShouldReturnSubmissionDatePlusTurnaroundTime() throws CalculateDueDateException {
@@ -28,6 +30,18 @@ class CalculateDueDate {
         LocalDateTime expectedResolveDate = LocalDateTime.of(2020, Month.AUGUST, 20, 16, 0);
 
         assertEquals(resolveDate, expectedResolveDate);
+    }
+
+    @Test
+    public void ShouldReturnCalculatedWorkdayDayWhenTurnaroundTimeIsWholeWorkday() throws CalculateDueDateException {
+
+        LocalDateTime resolveDateWithOneDay = issueTrackingSystemService.CalculateDueDate(SUBMISSION_DATE, TURNAROUND_TIME_FULL_WORKDAY);
+        LocalDateTime expectedResolveDateWithOneDay = LocalDateTime.of(2020, Month.AUGUST, 21, 9, 0);
+        assertEquals(resolveDateWithOneDay, expectedResolveDateWithOneDay);
+
+        LocalDateTime resolveDateWithTwoDays = issueTrackingSystemService.CalculateDueDate(SUBMISSION_DATE, TURNAROUND_TIME_TWO_FULL_WORKDAY);
+        LocalDateTime expectedResolveDateWithTwoDays = LocalDateTime.of(2020, Month.AUGUST, 22, 9, 0);
+        assertEquals(resolveDateWithTwoDays, expectedResolveDateWithTwoDays);
     }
 
 }
